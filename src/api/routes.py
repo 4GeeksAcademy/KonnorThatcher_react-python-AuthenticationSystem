396df4955a7f549aa0f9a92408e3a9edcfbfe224
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-08-11 01:19:48 +0000

    Wrote routes for signup, login & private with JWT

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
+from flask_jwt_extended import (create_access_token, get_jwt_identity, jwt_required)
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
@@ -16,3 +17,43 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/test', methods=['GET'])
+def token_maker():
+    return jsonify(
+        token=create_access_token("Is this a valid token?")
+    )
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = resquest.json
+    user = User.query.filter_by(email=data.get("email", None)).first()
+
+    if user:
+        return jsonify(message="This user already exists. Get outta here!"), 400
+    
+    user = User(**data)
+    db.session.add(user)
+    dp.session.commit()
+    return '', 204
+ 
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    user = User.query.filter_by(email=data.get("email", None)).first()
+
+    if not user or user.password != data.get("password", None):
+        return jsonify(message="Invalid Credentials"), 401
+
+    token = create_access_token(user.email)
+    return jsonify(token=token), 200
+
+@api.route('/private', methods=['GET'])
+@jwt_required()
+def top_secret():
+    user = User.query.filter_by(email=get_jwt_identity()).first()
+
+    return jsonify(
+        top_secret="https://image.tmdb.org/t/p/original/xsuEGorx2RcgtF6JKQjwYYrcr3E.jpg",
+        email=user.email
+    ), 204
--- src/app.py
+++ src/app.py
@@ -6,6 +6,8 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
@@ -18,6 +20,9 @@ ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+app.config["JWT_SECRET_KEY"] = "D8R434G43OG94wiggityERP9HwoogityVPF349"  # Change this!
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
